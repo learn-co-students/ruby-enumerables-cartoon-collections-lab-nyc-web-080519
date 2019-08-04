7a926c2d756c80a2765ebe93ffb1e631ae254afe
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 04:55:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |val,index| puts "#{index+1}. #{val}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{ |word| word.capitalize + "!"} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+  array.each do |i|
+    if i.length > 4 
+      return true
+    end 
+  end 
+  return false 
+end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#Given an array of strings (cheese_types), identify the first matching string in another array and return that string 
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |word|
+    if cheese_types.include?(word)
+      return word 
+    end 
+  end 
+  nil 
 end
